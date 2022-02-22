827f3c4a074071da7e0841970de438117c0a62f6
Matveev Timofey <justshowel@gmail.com>
2022-02-22 04:35:41 +0300

    feat: Set up sound for amazons

--- random-game/modules/classes/Amazon.js
+++ random-game/modules/classes/Amazon.js
@@ -12,6 +12,17 @@ export default class Amazon extends Pawn {
         this.pawnElement.classList.add("amazon");
     }
 
+    initializeSounds() {
+        super.initializeSounds();
+        this.soundPlayer.clearSounds('receiveDamageMelee');
+        this.soundPlayer.clearSounds('receiveDamageRanged');
+        this.soundPlayer.clearSounds('dealDamageMelee');
+        this.soundPlayer.addSound('receiveDamageMelee', "./assets/game/sounds/female_grunt.mp3", {volume: 0.6});
+        this.soundPlayer.addSound('receiveDamageRanged', "./assets/game/sounds/female_grunt.mp3", {volume: 0.6});
+        this.soundPlayer.addSound('dealDamageMelee', "./assets/game/sounds/Spin_attack.wav");
+
+    }
+
     dealDamage(targetPawn, attackType) {
         let adjacentEnemies = this.currentCell.getAdjacent().filter(c => c.pawn !== null && c.pawn.owner !== this.owner).map(c => c.pawn);
         adjacentEnemies.forEach(enemy => super.dealDamage(enemy, attackType));
